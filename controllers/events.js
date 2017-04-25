7bcd8a834286d92c153fac79593b6cf4257e8739
LBoyle <louis_boyle@hotmail.com>
2017-04-25 13:13:28 +0100

    got linking working from event to artist

--- controllers/events.js
+++ controllers/events.js
@@ -1,35 +1,13 @@
-// const Event = require('../models/event');
 const rp = require('request-promise');
 
-// function eventsShow(req, res) {
-//   Event
-//     .find()
-//     .exec()
-//     .then(events => {
-//       res.json(events);
-//     })
-//     .catch(err => {
-//       console.log('Route error: '+err);
-//     });
-// }
-
 function eventsOne(req, res) {
-  // Event
-  //   .findById(req.params.id)
-  //   .exec()
-  //   .then(event => {
-  //     res.render('events/event', {event});
-  //   })
-  //   .catch(err => {
-  //     console.log('Route error: '+err);
-  //   });
   const options = {
     uri: `https://app.ticketmaster.com/discovery/v2/events/${req.params.id}.json?apikey=sJdyvBFWmrZySkRDzJ2v0wNi2qtLb7pn`,
     headers: {
       'User-Agent': 'Request-Promise'
     },
     json: true
-  }; // event
+  };
   rp(options)
     .then(event => {
       console.log(event);
@@ -41,6 +19,5 @@ function eventsOne(req, res) {
 }
 
 module.exports = {
-  // show: eventsShow,
   showOne: eventsOne
 };
--- controllers/venues.js
+++ controllers/venues.js
@@ -1,28 +1,6 @@
-// const Venue = require('../models/venue');
 const rp = require('request-promise');
 
-// function venuesShow(req, res) {
-//   Venue
-//     .find()
-//     .exec()
-//     .then(venues => {
-//       res.json(venues);
-//     })
-//     .catch(err => {
-//       console.log('Route error: '+err);
-//     });
-// }
-
 function venuesOne(req, res) {
-  // Venue
-  //   .findById(req.params.id)
-  //   .exec()
-  //   .then(venue => {
-  //     res.render('venues/venue', {venue});
-  //   })
-  //   .catch(err => {
-  //     console.log('Route error: '+err);
-  //   });
   const options = {
     uri: `https://app.ticketmaster.com/discovery/v2/venues/${req.params.id}.json?apikey=sJdyvBFWmrZySkRDzJ2v0wNi2qtLb7pn`,
     headers: {
@@ -41,6 +19,5 @@ function venuesOne(req, res) {
 }
 
 module.exports = {
-  // show: venuesShow,
   showOne: venuesOne
 };
