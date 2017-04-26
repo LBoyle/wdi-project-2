47549779228ad91507e03640dcf44276f1935b2e
LBoyle <louis_boyle@hotmail.com>
2017-04-27 00:18:06 +0100

    added secure routes

--- config/routes.js
+++ config/routes.js
@@ -9,28 +9,47 @@ const sessions = require('../controllers/sessions');
 const users = require('../controllers/users');
 const favourites = require('../controllers/favourites');
 
+function secureRoute(req, res, next) {
+  if (!req.session.userId) {
+    return req.session.regenerate(() => {
+      res.redirect('/');
+    });
+  }
+  return next();
+}
+// function secureRoute(req, res, next) {
+//   if (!req.session.userId) {
+//     return req.session.regenerate(() => {
+//       req.flash('danger', 'You must be logged in.');
+//       res.redirect('/login');
+//     });
+//   }
+//
+//   return next();
+// }
+
 router.get('/', (req, res) => {
   // if (res.locals.isLoggedIn) res.locals.message = `Welcome back ${res.locals.user.username}`;
   res.render('statics/home');
 });
 
 router.route('/favourite/artist/:id')
-  .get(favourites.favArtist)
-  .delete(favourites.deleteArtist);
+  .get(secureRoute, favourites.favArtist)
+  .delete(secureRoute, favourites.deleteArtist);
 router.route('/favourite/event/:id')
-  .get(favourites.favEvent)
-  .delete(favourites.deleteEvent);
+  .get(secureRoute, favourites.favEvent)
+  .delete(secureRoute, favourites.deleteEvent);
 router.route('/favourite/venue/:id')
-  .get(favourites.favVenue)
-  .delete(favourites.deleteVenue);
+  .get(secureRoute, favourites.favVenue)
+  .delete(secureRoute, favourites.deleteVenue);
 
 router.route('/show/users')
-  .get(users.show);
+  .get(secureRoute, users.show);
 
 router.route('/account/user')
-  .delete(users.delete)
-  .put(users.change)
-  .get(users.showOne);
+  .delete(secureRoute, users.delete)
+  .put(secureRoute, users.change)
+  .get(secureRoute, users.showOne);
 
 router.route('/login')
   .post(sessions.login);
@@ -42,7 +61,7 @@ router.route('/register')
   .post(users.createUser);
 
 router.route('/account')
-  .get(users.account);
+  .get(secureRoute, users.account);
 
 // router.route('/artists')
 //   .get(artists.show);
